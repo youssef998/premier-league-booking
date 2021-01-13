566c1b5e529e5be9bdd4aed6c1afbfa4ab75ab50
youssef998 <47891555+youssef998@users.noreply.github.com>
2021-01-14 01:14:39 +0200

    sign-up,log-in,log-out requests

---
+++ modules/signup-manager.js
@@ -0,0 +1,21 @@
+exports.addNewAccount = function(newData, callback)
+{
+	accounts.findOne({user:newData.user}, function(e, o) {
+		if (o){
+			callback('username-taken');
+		}	else{
+			accounts.findOne({email:newData.email}, function(e, o) {
+				if (o){
+					callback('email-taken');
+				}	else{
+					saltAndHash(newData.pass, function(hash){
+						newData.pass = hash;
+					// append date stamp when record was created //
+						newData.date = moment().format('MMMM Do YYYY, h:mm:ss a');
+						accounts.insertOne(newData, callback);
+					});
+				}
+			});
+		}
+	});
+}
--- signupfinal.php
+++ signupfinal.php
@@ -46,6 +46,80 @@
             }
             
         }
+
+
+var AM = require('./modules/signup-manager');
+module.exports = function(app) {
+
+/*
+    login & logout
+*/
+
+    app.get('/', function(req, res){
+    // check if the user has an auto login key saved in a cookie //
+        if (req.cookies.login == undefined){
+            res.render('login', { title: 'Hello - Please Login To Your Account' });
+        }   else{
+    // attempt automatic login //
+            AM.validateLoginKey(req.cookies.login, req.ip, function(e, o){
+                if (o){
+                    AM.autoLogin(o.user, o.pass, function(o){
+                        req.session.user = o;
+                        res.redirect('/home');
+                    });
+                }   else{
+                    res.render('login', { title: 'Hello - Please Login To Your Account' });
+                }
+            });
+        }
+    });
+    
+    app.post('/', function(req, res){
+        AM.manualLogin(req.body['user'], req.body['pass'], function(e, o){
+            if (!o){
+                res.status(400).send(e);
+            }   else{
+                req.session.user = o;
+                if (req.body['remember-me'] == 'false'){
+                    res.status(200).send(o);
+                }   else{
+                    AM.generateLoginKey(o.user, req.ip, function(key){
+                        res.cookie('login', key, { maxAge: 900000 });
+                        res.status(200).send(o);
+                    });
+                }
+            }
+        });
+    });
+
+    // app.post('/logout', function(req, res){
+    //     res.clearCookie('login');
+    //     req.session.destroy(function(e){ res.status(200).send('ok'); });
+    // })
+
+
+
+
+app.get('/signup', function(req, res) {
+        res.render('signup', {  title: 'Signup' });
+    });
+    
+    app.post('/signup', function(req, res){
+        AM.addNewAccount({
+            name    : req.body['name'],
+            email   : req.body['email'],
+            user    : req.body['user'],
+            pass    : req.body['pass'],
+        }, function(e){
+            if (e){
+                res.status(400).send(e);
+            }   else{
+                res.status(200).send('ok');
+            }
+        });
+    
+    app.get('*', function(req, res) { res.render('404', { title: 'Page Not Found'}); });
+};
 </script>
 </head>
 <body >
