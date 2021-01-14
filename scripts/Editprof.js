fc8d4441b5d87be85a4c4e60746e475cc97a626a
youssef998 <47891555+youssef998@users.noreply.github.com>
2021-01-14 16:29:15 +0200

    mod

--- scripts/Editprof.js
+++ scripts/Editprof.js
@@ -3,7 +3,7 @@ function getUser(id)
     console.log(data);
     $.ajax({
         type:'GET',
-        url:'http://localhost:8080/user',
+        url:'http://localhost:8080/user?id='+id,
         success: function(result){
             console.log(result);
             var user = result;
@@ -11,18 +11,25 @@ function getUser(id)
         },
         error: function(xhr, status, error){
             var errorMessage = xhr.status + ': ' + xhr.statusText
-            alert('Error - ' + errorMessage);
-            
+            alert('Error - ' + errorMessage); 
         }
     });
     return null;
 }
 function loadEditFormValues()
 {
-    user=getUser();
+    id= getIdFromURL();
+    user=getUser(id);
     console.log("User is:",user.username);
     document.getElementById('first').value = user.firstName;
     document.getElementById('last').value = user.lastName;
     document.getElementById('mail').value = user.email;
     document.getElementById('address').value = user.city;
+}
+function getIdFromURL()
+{
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+    console.log("myParam:",id);
+    return id;
 }
