abdf993ea251038e20be450e3dabd07b0c040dfb
youssef998 <47891555+youssef998@users.noreply.github.com>
2021-01-14 13:50:11 +0200

    editprof

---
+++ editprof.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+
+
+<html lang="en">
+<head>
+    <title>Edit Profile</title>
+
+    <style type="text/css">
+
+    </style>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
+    <link rel="stylesheet" type="text/css" href="css/index.css" >
+    <script src=http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js> </script>
+    <script type="text/javascript">
+    </script>
+</head>
+<body >
+    <link rel="stylesheet" href="index.css">
+    <div class="container rounded bg-white mt-5 mb-5">
+            <div class="p-5 py-7">
+                <div class="d-flex justify-content-between align-items-center mb-3">
+                    <h4 class="text-right">Profile Settings</h4>
+                </div>
+                <div class="row mt-2">
+                    <div class="col-md-6"><input type="text" class="form-control" placeholder="First Name" value=""></div>
+                    <div class="col-md-6"><input type="text" class="form-control" value="" placeholder="Last Name"></div>
+                </div>
+                <div class="row mt-3">               
+                    <div class="col-md-12"><input type="text" class="form-control" placeholder="Email" value=""></div>
+                </div>
+                <div class="row mt-3"> 
+                    <div class="col-md-12"><input type="text" class="form-control" placeholder="Address" value=""></div>
+                </div>
+                <div class="row mt-3">               
+                    <div class="col-md-12"><input type="text" class="form-control" placeholder="Password" value=""></div>
+                   </div>
+                   <div class="row mt-3">  
+                    <div class="col-md-12"><input type="text" class="form-control" placeholder="Confirm Password" value=""></div>
+                </div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+            </div>
+        </div>
+    </div>
+</div>
+</div>
+</body>
+</html>
