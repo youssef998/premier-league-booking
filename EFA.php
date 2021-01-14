f0a613f72e2d19319ba85dc8b4e6e5678a493a9e
KarimIbrahim11 <karim.ibrahim.amin@gmail.com>
2021-01-14 05:01:33 +0200

    Added navbar to a file.  Working on editing matches
    commented the echo $user line temporarily in navbar.php

--- EFA.php
+++ EFA.php
@@ -2,13 +2,13 @@
 <html lang="en">
     <head>
         <title>EFA Management</title>
-        <!-- Latest compiled and minified CSS -->
+<!--         
+        <link rel="stylesheet" type="text/css" href="index.css" > -->
+        <script src=http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js> </script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
-        <!-- Optional theme -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
 
-        <!-- Latest compiled and minified JavaScript -->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
        
         <link rel="stylesheet" type="text/css" href="EFA.css">
@@ -24,9 +24,16 @@
 
 
     </head>
+    
     <body>
+        <?php include_once('navbar.php'); ?>
+        <div class="efacontainer">
         <form method="POST"  action="" class="CreateMatchContainer" onsubmit="return onSubmit()">		
-            <h3>Create Match</h3>
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>Create Match</h2>
+                </div>
+            </div>
             <div>
                 <label >Home Team:</label>
                 <?php
@@ -82,7 +89,78 @@
             </div>
         </form>
         <div>
-            
+        <div class="MatchDetailsContainer">
+            <div class="row">
+                <div class="col-sm-6">
+                    <h2>List Products</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-6">
+                    <table id="MatchesTable" class="table table-bordered table-condensed table-striped">
+                        <thead>
+                            <tr>
+                                <th>Edit</th>
+                                <th>Home Team</th>
+                                <th>Away Team</th>
+                                <th>Venue/Stadium</th>
+                                <th>Date/Time</th>
+                                <th>Referee</th>
+                                <th>First Linesman</th>
+                                <th>Second Lines Man</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                    </table>
+                </div>
+            </div>
         </div>
+        <!--
+        <table>
+            <tr>
+                <th>Home Team</th>
+                <th>Away Team</th>
+                <th>Venue/Stadium</th>
+                <th>Date/Time</th>
+                <th>Referee</th>
+                <th>First Linesman</th>
+                <th>Second Lines Man</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+            <tr>
+                <td>Alfreds Futterkiste</td>
+                <td>Maria Anders</td>
+                <td>Germany</td>
+            </tr>
+            <tr>
+                <td>Centro comercial Moctezuma</td>
+                <td>Francisco Chang</td>
+                <td>Mexico</td>
+            </tr>
+            <tr>
+                <td>Ernst Handel</td>
+                <td>Roland Mendel</td>
+                <td>Austria</td>
+            </tr>
+            <tr>
+                <td>Island Trading</td>
+                <td>Helen Bennett</td>
+                <td>UK</td>
+            </tr>
+            <tr>
+                <td>Laughing Bacchus Winecellars</td>
+                <td>Yoshi Tannamuri</td>
+                <td>Canada</td>
+            </tr>
+            <tr>
+                <td>Magazzini Alimentari Riuniti</td>
+                <td>Giovanni Rovelli</td>
+                <td>Italy</td>
+            </tr>
+        </table>
+                    
+        </div>-->
+    </div>
     </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,35 @@
+
+<style type="text/css">
+nav{
+	background: #016064;
+	height: 80px;
+	width: 100%;
+}
+nav ul{
+	float: right;
+	margin-right: 20px;
+}
+nav li{
+	display: inline-block;
+	line-height: 80px;
+	margin:0 5px;
+}
+nav ul li a{
+color: white;
+font-size: 17px;
+padding: 7px 13px;
+border-radius: 3px;
+text-transform: uppercase;
+}
+</style>
+<nav>
+    <ul>
+        <!-- <li><a><?php echo $user;?></a></li> -->
+        <li><a href="home.php">Home</a></li>
+        <li><a href="#">Sports</a></li>
+        <li><a href="editprof.php">Edit Profile</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="regalto.php">Sign out</a></li>
+        <li><a href="Efa.php">EFA</a></li>
+    </ul>
+</nav>
