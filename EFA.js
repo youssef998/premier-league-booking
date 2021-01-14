f78373f003ddef434e7fdf9ae06bef8a1fede4d1
KarimIbrahim11 <karim.ibrahim.amin@gmail.com>
2021-01-14 05:50:22 +0200

    Working on match details an dCreate Request

--- EFA.js
+++ EFA.js
@@ -3,9 +3,8 @@ function validateForm() {
     var awayteam = document.getElementById('f_awayteam').value;
     // console.log(hometeam);
     // console.log(awayteam);
-    if (hometeam == awayteam)
-    {
-        displayError('awayTeamError',"Please choose two different teams");
+    if (hometeam == awayteam) {
+        displayError('awayTeamError', "Please choose two different teams");
         return false;
     }
     return true;
@@ -13,7 +12,33 @@ function validateForm() {
 function onSubmit() {
     var valid = validateForm();
     if (valid) {
-       console.log('SUBMIT BA2A YASTA ');
+        // TODO ADD THE POST REQUEST AND ALERT IF OK
+        const axios = require('axios');
+
+        axios.post('https://api.randomservice.com/dog',
+            {
+                "homeTeam": "Ahly",
+                "awayTeam": "Zamalek",
+                "stadium": "Borg El arab Stadium",
+                "dateTime": "2018-03-29T11:34:00.000Z",
+                "referee": "Mostafa Sherif",
+                "linesman1": "Karim Ibrahim",
+                "linesman2": "Youssef Sayed",
+
+            },
+            {
+                headers:
+                {
+                    'content-type': 'application/json',
+                    authorization: 'Bearer 123abc456def'
+                }
+            }).then((res) => {
+                console.log(res.data)
+            }).catch((error) => {
+                console.error(error)
+            })
+
+
     }
     return valid;
 }
